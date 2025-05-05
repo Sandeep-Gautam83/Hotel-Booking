50d9eba26fcc8a2ee721cbcc6a03fef2bf983e43
sandeep <sundeep512pbh@gmail.com>
2025-05-05 19:34:51 +0530

    uploading project

---
+++ hotel-react-frontend/src/component/common/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () =>{
+
+    return(
+        <footer className="my-footer">
+            <span> The Legend Hotel | All Rights Reserved &copy; 2025</span>
+        </footer>
+    )
+
+}
+export default Footer;
